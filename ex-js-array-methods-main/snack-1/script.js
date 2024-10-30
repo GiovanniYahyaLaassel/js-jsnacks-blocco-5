5883c4e88c8dbf54c2d97ec9f5d3b43e836a1734
Laassel <lassel.giovanni@gmail.com>
2024-10-30 15:50:02 +0100

    Snack-1

--- ex-js-array-methods-main/snack-1/script.js
+++ ex-js-array-methods-main/snack-1/script.js
@@ -2,3 +2,18 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+//metodo con il ciclo for
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
+
+
+// Metodo con forEach
+names.forEach(function(name) {
+    console.log(name);
+}) 
+
+//Metodo forEach con arrow function
+
+names.forEach(name => console.log(name));

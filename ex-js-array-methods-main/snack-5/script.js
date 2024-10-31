a397ef22edc71150122ee02953d63d66f2fe12af
Laassel <lassel.giovanni@gmail.com>
2024-10-31 15:42:17 +0100

    snack-5

--- ex-js-array-methods-main/snack-5/script.js
+++ ex-js-array-methods-main/snack-5/script.js
@@ -2,3 +2,13 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
+// Uso il metodo filter
+// const evenNubers = nums.filter(function(num){
+//     return num % 2 === 0;
+// });
+// console.log(evenNubers);
+
+// Uso il metedo filter con l'arrow function
+const evenNubers = nums.filter(num => num % 2 === 0);
+console.log(evenNubers);
